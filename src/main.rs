8d7395773b72d585bcb933b0625dab2ef0210b94
opyatmaminaradost-arch <opyatmaminaradost@gmail.com>
2026-08-31 18:45:46 +0700

    Add config resolver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
 use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
 
-fn main(){
-    let dir_of_file = File::open(r#"video.mp4"#);
+fn main() -> io::Result<()>{
+
+    if let Some(file) = find_config_values("file.link")? {
+        let metadata = file.metadata()?;
+
+        println!("Size: {}", metadata.len());
+        println!("Is file: {}", metadata.is_file());
+    }
+
+    Ok(())
+
+}
+
+fn find_config_values(pattern: &str) -> io::Result<Option<File>> {
+
+    let config_lines = File::open("config.properties").unwrap();
+    let reader = BufReader::new(config_lines);
+
+    for line in reader.lines() {
+        let line = line?;
+
+        if let Some((key,value)) = line.split_once('='){
+            if key.trim() == pattern {
+                let path = value
+                        .trim()
+                        .trim_matches('"');
+
+                let file = File::open(path)?;
+
+                return Ok(Some(file));
+            }
+        }
+    }
+    Ok(None)
 }
