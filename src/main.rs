9fd59c00510c7616e287c809a340b1408b34b6c2
opyatmaminaradost-arch <opyatmaminaradost@gmail.com>
2026-08-31 19:31:22 +0700

    All file in buffer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 
-fn main() -> io::Result<()>{
-
-    if let Some(file) = find_config_values("file.link")? {
-        let metadata = file.metadata()?;
-
-        println!("Size: {}", metadata.len());
-        println!("Is file: {}", metadata.is_file());
-    }
-
-    Ok(())
-
+fn main() {
+    check_all_boxes_in_mp4().unwrap();
 }
 
 fn find_config_values(pattern: &str) -> io::Result<Option<File>> {
 
-    let config_lines = File::open("config.properties").unwrap();
+    let config_lines = File::open("config.properties")?;
     let reader = BufReader::new(config_lines);
 
     for line in reader.lines() {
@@ -36,4 +27,19 @@ fn find_config_values(pattern: &str) -> io::Result<Option<File>> {
         }
     }
     Ok(None)
+}
+
+
+fn check_all_boxes_in_mp4() -> io::Result<()> {
+
+    if let Some(mut file) = find_config_values("file.link")? {
+
+        let mut buff = Vec::new();
+
+        file.read_to_end(&mut buff)?;
+
+        println!("Readed: {}", buff.len());
+    }
+
+    Ok(())
 }
